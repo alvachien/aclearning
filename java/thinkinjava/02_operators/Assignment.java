87718b460a522bab96c7cf93d3d4e4d37e06ea40
Alva Chien <alvachien@163.com>
2017-03-06 18:57:33 +0800

    Continues the learning

---
+++ java/thinkinjava/02_operators/Assignment.java
@@ -0,0 +1,15 @@
+class Tank {
+    int level;
+}
+
+public class Assignment  {
+
+    public static void main(String[] args) {
+        Tank t1 = new Tank();
+        Tank t2 = new Tank();
+        t1.level = 9;
+        t2.level = 47;
+
+    }
+   
+}
