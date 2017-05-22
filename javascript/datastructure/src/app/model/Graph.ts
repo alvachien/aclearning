5215455b410059a0cd103090b2d1725595750aaa
Alva Chien <alvachien@163.com>
2017-05-22 18:28:54 +0800

    Add new changes

--- javascript/datastructure/src/app/model/Graph.ts
+++ javascript/datastructure/src/app/model/Graph.ts
@@ -14,4 +14,20 @@ export class Graph<T> {
         this._adjList.get(v).push(w);
         this._adjList.get(w).push(v);
     }
+
+    public toString(): string {
+        let s:string = '';
+        for(let i = 0; i < this._vertices.length; i ++) {
+            s += this._vertices[i] + ' -> ';
+
+            let neighbors = this._adjList.get(this._vertices[i]);
+            for(let j = 0; j < neighbors.length; j++) {
+                s += neighbors[j] + ' ';
+            }
+
+            s += '\n';
+        }
+
+        return s;
+    }
 }
